034638583fc60774309540920ce5ff4022f84b00
1boveAllBelow1 <franzharvey80@gmail.com>
2025-06-17 00:04:36 +0800

    Add files via upload

--- database/payroll_db.sql
+++ database/payroll_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 14, 2025 at 11:48 AM
+-- Generation Time: Jun 16, 2025 at 06:04 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -44,13 +44,25 @@ INSERT INTO `admin` (`admin_id`, `admin_pass`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `employee`
+-- Table structure for table `classes`
 --
 
-CREATE TABLE `employee` (
-  `employee_id` int(10) NOT NULL,
-  `employee_pass` varchar(255) NOT NULL
+CREATE TABLE `classes` (
+  `class_name` varchar(10) NOT NULL,
+  `Salary` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `classes`
+--
+
+INSERT INTO `classes` (`class_name`, `Salary`) VALUES
+('fgfgf', 45),
+('sds', 34),
+('erere', 34),
+('sds', 2),
+('ere', 34),
+('tty', 34);
 
 -- --------------------------------------------------------
 
@@ -63,9 +75,45 @@ CREATE TABLE `employee_credentials` (
   `full_name` varchar(255) NOT NULL,
   `email` varchar(100) NOT NULL,
   `mobile_no` int(100) NOT NULL,
+  `class_name` varchar(10) NOT NULL,
   `branch` varchar(15) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employee_credentials`
+--
+
+INSERT INTO `employee_credentials` (`employee_id`, `full_name`, `email`, `mobile_no`, `class_name`, `branch`, `password`) VALUES
+(2025001, 'John Doe', 'hwuehdwhw@yahoo.com', 6666666, '', 'IT', '$2y$10$5Fl7CGKO1dvDrPeCC/fWY.woP/gRLe9njGRTVz78deF32/DQOBbaq');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_payroll`
+--
+
+CREATE TABLE `employee_payroll` (
+  `payroll_id` int(11) NOT NULL,
+  `employee_id` int(10) NOT NULL,
+  `class_name` varchar(100) DEFAULT NULL,
+  `base_salary` decimal(10,2) DEFAULT NULL,
+  `sss_deduction` decimal(10,2) DEFAULT NULL,
+  `philhealth_deduction` decimal(10,2) DEFAULT NULL,
+  `pagibig_deduction` decimal(10,2) DEFAULT NULL,
+  `taxable_income` decimal(10,2) DEFAULT NULL,
+  `tax` decimal(10,2) DEFAULT NULL,
+  `net_income` decimal(10,2) DEFAULT NULL,
+  `payroll_month` int(11) NOT NULL,
+  `payroll_year` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employee_payroll`
+--
+
+INSERT INTO `employee_payroll` (`payroll_id`, `employee_id`, `class_name`, `base_salary`, `sss_deduction`, `philhealth_deduction`, `pagibig_deduction`, `taxable_income`, `tax`, `net_income`, `payroll_month`, `payroll_year`) VALUES
+(3, 2025001, '1', 17100.00, 769.50, 855.00, 400.00, 15075.50, 0.00, 12060.40, 2, 2024);
 
 --
 -- Indexes for dumped tables
@@ -75,7 +123,15 @@ CREATE TABLE `employee_credentials` (
 -- Indexes for table `employee_credentials`
 --
 ALTER TABLE `employee_credentials`
-  ADD PRIMARY KEY (`employee_id`);
+  ADD PRIMARY KEY (`employee_id`),
+  ADD UNIQUE KEY `employee_id` (`employee_id`);
+
+--
+-- Indexes for table `employee_payroll`
+--
+ALTER TABLE `employee_payroll`
+  ADD PRIMARY KEY (`payroll_id`),
+  ADD KEY `employee_id` (`employee_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -86,6 +142,22 @@ ALTER TABLE `employee_credentials`
 --
 ALTER TABLE `employee_credentials`
   MODIFY `employee_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2025006;
+
+--
+-- AUTO_INCREMENT for table `employee_payroll`
+--
+ALTER TABLE `employee_payroll`
+  MODIFY `payroll_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `employee_payroll`
+--
+ALTER TABLE `employee_payroll`
+  ADD CONSTRAINT `employee_payroll_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employee_credentials` (`employee_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
